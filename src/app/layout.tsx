79030f183fcc9e5f454fecf6aacf526f2fd12ac3
liamwebb0 <liam.wweebb@gmail.com>
2025-08-01 18:00:13 +0200

    restart project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="h-full"> 
+      
+      <body className={cn("relative h-full font-sans antialiased")}>
+        <main className='relative flex flex-col min-h-screen'>
+          <div>{children}</div>
+        </main>
       </body>
     </html>
   );
